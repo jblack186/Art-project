1632133eee64957114541e9bc3310ffe18346e7a
Jamison Blackwell <jamie@Jamisons-MacBook-Pro.local>
2020-12-22 10:58:28 -0500

    user can add multiple items now

--- pages/api/checkout/session.tsx
+++ pages/api/checkout/session.tsx
@@ -1,20 +1,28 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+
 import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2020-08-27',
 });
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  
   const {quantity} = req.body;
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     line_items: [
       {
-        price: process.env.PRICE_ID,
+        price: process.env.PRICE_ID_BRUSHES, 
         quantity,
 
       },
+      {
+        price: process.env.PRICE_ID_FLOWERS, 
+        quantity,
+
+      },
+
     ],
     mode: 'payment',
     success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
